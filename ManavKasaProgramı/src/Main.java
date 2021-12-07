a32ad8dbf08c0b79412e31829ccaae6459f5195f
Umut <55003938+umutgecer@users.noreply.github.com>
2021-12-07 23:35:56 +0300

    Add files via upload

---
+++ ManavKasaProgramı/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Domatezin Kilosunu Hesaplama
+        float domateskilo = (float) 1.50;
+        System.out.println("Armut Kaç Kilo" + armut);
+        float armut;
+        float dsonuç = domateskilo * dkackilo ;
+
+    }
+}
