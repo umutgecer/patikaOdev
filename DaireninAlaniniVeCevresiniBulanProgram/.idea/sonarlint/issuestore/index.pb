8e3dd605b88a1718492d3663f4eddee358f8d583
umutgecer <umutgecer@outlook.com>
2022-06-12 16:26:32 +0300

    switch case hesap makinesi ödevi

---
+++ DaireninAlaniniVeCevresiniBulanProgram/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+L
+.idea/codeStyles/Project.xml,6\e\6ecd6000a7b6f4a2884412ff19f74193ed089648
+T
+$.idea/codeStyles/codeStyleConfig.xml,d\d\dd7b3350c29c49497756c7f32c69a3bed18b1cba
+Z
+*DaireninAlaniniVeCevresiniBulanProgram.iml,b\4\b4e6b55a2155e19d59027e23234f0026cbedd1b7
+=
+
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
---
+++ switchCase-HesapMakinesi/src/main/java/HesapMakinesi.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class HesapMakinesi {
+    public static void main(String[] args) {
+        int s1, s2, islem;
+
+        Scanner sayiAl = new Scanner(System.in);
+        System.out.print("Birinci Sayiyi Giriniz: ");
+        s1 = sayiAl.nextInt();
+        System.out.print("İkinci Sayiyi Giriniz: ");
+        s2 = sayiAl.nextInt();
+
+        System.out.print("1.Toplama\n2.Çıkarma\n3.Çarpma\n4.Bölme \n Yapılacak İşlemi Seçiniz: ");
+        islem = sayiAl.nextInt();
+
+        switch (islem) {
+            case 1:
+                System.out.println(s1 + " ile " + s2 + " sayisinin toplamı: " + (s1 + s2));
+                break;
+            case 2:
+                System.out.println(s1 + " ile " + s2 + " sayisinin toplamı: " + (s1 - s2));
+                break;
+            case 3:
+                System.out.println(s1 + " ile " + s2 + " sayisinin toplamı: " + (s1 * s2));
+                break;
+            case 4:
+                System.out.println(s1 + " ile " + s2 + " sayisinin toplamı: " + (s1 / s2));
+                break;
+            default:
+                System.out.println("Girilen Değer Geçersiz Lütfen Tekrar Deneyeniz");
+                break;
+
+        }
+    }
+}
